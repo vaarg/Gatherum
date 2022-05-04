cd5d76a7c5cf175cd90901d347c29fe2370e52e8
vaarg <104344158+vaarg@users.noreply.github.com>
2022-05-04 22:17:53 +1000

    Update gatherum.sh

--- gatherum.sh
+++ gatherum.sh
@@ -6,6 +6,7 @@ TEMP=$PWD/"tmp.txt"
 GREEN="\e[32m"
 CYAN="\e[36m"
 RED="\e[31m"
+BOLD="\e[1m"
 ENDCOLOR="\e[0m"
 
 function port-check() {
@@ -39,28 +40,42 @@ function port() {
     fi
 }
 
+function clean-up() {
+    rm tmp.txt
+    rm openPorts.txt
+}
+
 awk '/open/ {print}' tmp.txt > openPorts.txt
-echo -e "\e[1m\r\nOPEN PORTS DISCOVERED:\r\n${ENDCOLOR}"
+cat openPorts.txt | grep "open" > /dev/null 2>&1
+if [ "$?" -eq 0 ]
+then
+    echo -e "${BOLD}\r\nOPEN PORTS DISCOVERED:\r\n${ENDCOLOR}"
+else
+    echo -e "${RED}\r\n[-] NO OPEN PORTS DETECTED!${ENDCOLOR}"
+    clean-up
+    exit 1
+fi
+
 while read -r line
 do
     echo -e "${CYAN}[+] "$line"${ENDCOLOR}" 
 done < openPorts.txt
-echo -e "\e[1m\r\nBeginning Enumeration...${ENDCOLOR}"
+echo -e "${BOLD}\r\nBeginning Enumeration...${ENDCOLOR}"
 
 port 21 --script=ftp-anon.nse FTP
-port 22 --script=ssh2-enum-algos.nse SSH # Check/adds scripts!
-port 25 --script=smtp-enum-users.nse SMTP # Check/adds scripts!
+port 22 --script=ssh2-enum-algos.nse SSH # Check/add scripts!
+port 23 --script=telnet-ntlm-info.nse Telnet
+port 25 --script=smtp-enum-users.nse SMTP # Check/add scripts!
 port 53 --script=dns-brute.nse DNS
 port 80 --script=http-enum.nse,http-wordpress-enum.nse,http-backup-finder HTTP
-port 110 --script=pop3-ntlm-info.nse,pop3-capabilities.nse POP3 # Check/adds scripts!
-port 143 --script=imap-ntlm-info.nse,imap-capabilities.nse IMAP # Check/adds scripts!
-port 443 --script=http-enum.nse,http-wordpress-enum.nse,http-backup-finder HTTPS # Check/adds scripts!
+port 110 --script=pop3-ntlm-info.nse,pop3-capabilities.nse POP3 # Check/add scripts!
+port 143 --script=imap-ntlm-info.nse,imap-capabilities.nse IMAP # Check/add scripts!
+port 443 --script=http-enum.nse,http-wordpress-enum.nse,http-backup-finder HTTPS # Check/add scripts!
 port 445 --script=smb-enum-shares.nse,smb-enum-users.nse SMB
-port 465 --script=smtp-enum-users.nse SMTPS # Check/adds scripts!
-port 993 --script=imap-ntlm-info.nse,imap-capabilities.nse IMAPS # Check/adds scripts!
-port 995 --script=pop3-ntlm-info.nse,pop3-capabilities.nse POP3S # Check/adds scripts!
-port 3389 --script=rdp-enum-encryption.nse RDP # Check/adds scripts!
+port 465 --script=smtp-enum-users.nse SMTPS # Check/add scripts!
+port 993 --script=imap-ntlm-info.nse,imap-capabilities.nse IMAPS # Check/add scripts!
+port 995 --script=pop3-ntlm-info.nse,pop3-capabilities.nse POP3S # Check/add scripts!
+port 3389 --script=rdp-enum-encryption.nse RDP # Check/add scripts!
 
-rm tmp.txt
-rm openPorts.txt
+cleanup
 exit 0
