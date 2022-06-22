68324ac8a6a4f34dd7d0793af650d17593eb6be7
vaarg <104344158+vaarg@users.noreply.github.com>
2022-06-22 22:24:31 +1000

    Update gatherum.sh

--- gatherum.sh
+++ gatherum.sh
@@ -63,19 +63,19 @@ done < openPorts.txt
 echo -e "${BOLD}\r\nBeginning Enumeration...${ENDCOLOR}"
 
 port 21 --script=ftp-anon.nse FTP
-port 22 --script=ssh2-enum-algos.nse SSH # Check/add scripts!
+port 22 --script=ssh2-enum-algos.nse SSH #
 port 23 --script=telnet-ntlm-info.nse Telnet
-port 25 --script=smtp-enum-users.nse SMTP # Check/add scripts!
+port 25 --script=smtp-enum-users.nse SMTP #
 port 53 --script=dns-brute.nse DNS
 port 80 --script=http-enum.nse,http-wordpress-enum.nse,http-backup-finder HTTP
-port 110 --script=pop3-ntlm-info.nse,pop3-capabilities.nse POP3 # Check/add scripts!
+port 110 --script=pop3-ntlm-info.nse,pop3-capabilities.nse POP3 #
 port 111 --script=nfs-ls,nfs-statfs,nfs-showmount NFS
-port 143 --script=imap-ntlm-info.nse,imap-capabilities.nse IMAP # Check/add scripts!
-port 443 --script=http-enum.nse,http-wordpress-enum.nse,http-backup-finder HTTPS # Check/add scripts!
+port 143 --script=imap-ntlm-info.nse,imap-capabilities.nse IMAP #
+port 443 --script=http-enum.nse,http-wordpress-enum.nse,http-backup-finder HTTPS #
 port 445 --script=smb-enum-shares.nse,smb-enum-users.nse SMB
-port 465 --script=smtp-enum-users.nse SMTPS # Check/add scripts!
-port 993 --script=imap-ntlm-info.nse,imap-capabilities.nse IMAPS # Check/add scripts!
-port 995 --script=pop3-ntlm-info.nse,pop3-capabilities.nse POP3S # Check/add scripts!
+port 465 --script=smtp-enum-users.nse SMTPS #
+port 993 --script=imap-ntlm-info.nse,imap-capabilities.nse IMAPS #
+port 995 --script=pop3-ntlm-info.nse,pop3-capabilities.nse POP3S #
 port 3389 --script=rdp-enum-encryption.nse RDP # Check/add scripts!
 
 clean-up
